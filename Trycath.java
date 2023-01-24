5d188bbd3164d3cfadc96742f59ef9b8800f3689
gurjarbc <121430027+gurjarbc@users.noreply.github.com>
2023-01-24 14:09:28 +0530

    Add files via upload
    comit final

---
+++ Trycath.java
@@ -0,0 +1,16 @@
+public class Trycath {
+           
+  void bs(){
+    try {
+     String name="bhagchand";
+    System.out.println("hello " +name);
+}
+
+ catch (Exception e) {
+    System.out.println(e);
+}}
+   public  static void main(String[]args) {
+    Trycath ab=new Trycath();
+    ab.bs();
+   } 
+}
